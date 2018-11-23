e8aff8e45a6aa64447bd7e8b04d560a5c8c974fc
Joost De Cock <joost@decock.org>
2018-11-23 14:07:20 +0100

    :sparkles: Added remark plugin for extenal links

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
       options: {
         plugins: [
           "gatsby-remark-autolink-headers",
+          "gatsby-remark-external-links",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants",
           {
