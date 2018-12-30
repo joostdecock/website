79b40e781204f5c83920f865b399c2876b6c567c
Joost De Cock <joost@decock.org>
2018-12-30 22:56:32 +0100

    :fire: Moved ChoosePattern component to ChoosePattern page

--- src/components/pages/ChoosePattern.js
+++ src/components/pages/ChoosePattern.js
@@ -1,12 +1,34 @@
 import React from "react";
 import BaseLayout from "../layouts/Base";
 import AuthContainer from "../app/auth/Container";
-import ChoosePattern from "../app/draft/ChoosePattern";
+import { FormattedMessage } from "react-intl";
+import Breadcrumbs from "../Breadcrumbs";
+import PatternPicker from "../app/draft/PatternPicker";
+import { patternList } from "@freesewing/patterns";
 
 const ChoosePatternPage = props => (
   <BaseLayout>
     <AuthContainer>
-      <ChoosePattern {...props.pageContext} />
+      <div>
+        <Breadcrumbs
+          via={[{ label: "app.newDraft", link: false }]}
+          towards={["app.chooseAModel", "app.configureYourDraft"]}
+        >
+          <FormattedMessage id="app.chooseAPattern" />
+        </Breadcrumbs>
+        <h1 className="txt-center">
+          <small className="color-muted">
+            <FormattedMessage id="app.newDraft" />:
+          </small>
+          <br />
+          <FormattedMessage id="app.chooseAPattern" />
+        </h1>
+        <div className="wrap narrow">
+          <div className="overpad1-always">
+            <PatternPicker patterns={patternList} {...props.pageContext} />
+          </div>
+        </div>
+      </div>
     </AuthContainer>
   </BaseLayout>
 );
